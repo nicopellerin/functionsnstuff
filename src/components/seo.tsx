30ffcb757c5fb83d6b0ce1151190221fb9745bce
Nico Pellerin <hello@nicopellerin.io>
2020-04-30 21:10:15 -0400

    Fix twitter card data2

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -56,7 +56,7 @@ function SEO({ description, lang, meta, title, image }) {
         },
         {
           name: `twitter:card`,
-          content: ogImage,
+          content: `summary`,
         },
         {
           name: `twitter:creator`,
