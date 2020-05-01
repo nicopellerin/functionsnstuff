ddccaa2cd310eae7528fce1e0fce3444a97ce1cb
Nico Pellerin <hello@nicopellerin.io>
2020-04-30 20:13:49 -0400

    Fix no tutorial bug2

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,13 +16,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: "tutorials",
-        path: `${__dirname}/src/tutorials`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: "tutorials",
+    //     path: `${__dirname}/src/tutorials`,
+    //   },
+    // },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
