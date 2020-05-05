9968b562c58a5137226ee1af6149e3aacc209978
Nico Pellerin <hello@nicopellerin.io>
2020-05-05 13:27:00 -0400

    Add native sharing to mobile

--- src/components/share.tsx
+++ src/components/share.tsx
@@ -8,12 +8,50 @@ import {
   EmailIcon,
 } from "react-share"
 import styled from "styled-components"
+import { FiShare2 } from "react-icons/fi"
 
 const Share = () => {
+  const isMobile = (width = 1024) => {
+    let mql =
+      typeof window !== "undefined" &&
+      window.matchMedia(`(max-width: ${width}px)`)
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        typeof window !== "undefined" && window.navigator.userAgent
+      ) &&
+      mql.matches
+    ) {
+      return true
+    }
+    return false
+  }
+
   const url = typeof window !== "undefined" ? window.location.href : ""
+
+  const mobileNativeShare = () => {
+    const navigatorShare =
+      typeof window !== "undefined" && window.navigator.share
+
+    if (navigatorShare) {
+      navigatorShare({
+        title: document.title,
+        text: document
+          .querySelector('meta[name="description"]')
+          ?.getAttribute("content"),
+        url,
+      })
+    }
+  }
 
   return (
     <Wrapper>
+      {isMobile() ? (
+        <FiShare2
+          size={32}
+          onClick={mobileNativeShare}
+          style={{ marginRight: "1.5rem" }}
+        />
+      ) : null}
       <FacebookShareButton url={url}>
         <FacebookIcon
           style={{
