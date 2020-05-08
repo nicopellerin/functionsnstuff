47b1c7efe9ce111511615f48c95a39afce941b09
Nico Pellerin <hello@nicopellerin.io>
2020-05-08 17:09:05 -0400

    Add header to 404 page2

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,12 +5,14 @@ import SEO from "../components/seo"
 import PageHeader from "../components/page-header"
 
 const NotFoundPage = () => (
-  <Layout>
+  <>
     <SEO title="404: Not found" />
     <PageHeader title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <Layout>
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Layout>
+  </>
 )
 
 export default NotFoundPage
