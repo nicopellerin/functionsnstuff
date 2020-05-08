b0e4390c40363dc734f04485aa658b0500eed302
Nico Pellerin <hello@nicopellerin.io>
2020-05-08 16:58:02 -0400

    Add header to 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,10 +2,12 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import PageHeader from "../components/page-header"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
+    <PageHeader title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
