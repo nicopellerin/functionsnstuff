89ddd20f45bd4c8e5c811069c741e339baabb311
Nico <nickypellerin@gmail.com>
2020-05-24 19:36:21 -0400

    Remove comments browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,30 +1,12 @@
 const React = require("react")
-// const ReactDOM = require("react-dom")
 const Background = require("./src/components/background").default
 
 require("typeface-lora")
 
-// const LazyBackground = React.lazy(() => {
-//   return new Promise(resolve => {
-//     setTimeout(() => resolve(import("./src/components/background")), 1100)
-//   })
-// })
-
-// exports.replaceHydrateFunction = () => {
-//   return (element, container, callback) => {
-//     ReactDOM.unstable_createRoot(container, {
-//       hydrate: true,
-//       hydrationOptions: { onHydrated: callback },
-//     }).render(element)
-//   }
-// }
-
 exports.wrapRootElement = ({ element }) => {
   return (
     <>
-      {/* <React.Suspense fallback={<div></div>}> */}
       <Background />
-      {/* </React.Suspense> */}
       <React.Fragment>{element}</React.Fragment>
     </>
   )
